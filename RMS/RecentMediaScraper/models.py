8aa3dbb11e42eb681fc2b421c4fc2c67ef7a241d
danlock <d.f.locked@gmail.com>
2015-04-07 18:19:07 -0300

    partially defined models, still havent run migrations

--- RMS/RecentMediaScraper/models.py
+++ RMS/RecentMediaScraper/models.py
@@ -1,18 +1,31 @@
 from django.db import models
+import json
 
 # Create your models here.
 
 class Movie(models.Model):
-    posters
-    year
-    runtime
-    abridged_cast
-    mpaa_rating
-    links
-    alternate_ids
-    release_dates
-    title
-    id
-    critics_consensus
-    ratings
-    synopsis
+    posters = models.URLField()
+    year = models.IntegerField()
+    runtime = models.IntegerField()
+    #abridged_cast
+    cast = models.TextField()
+    mpaa_rating = models.CharField(max_length=5)
+    links = models.URLField()
+    #id
+    rt_id = models.IntegerField()
+    #alternate_ids.imdb
+    imdb_id = models.IntegerField()
+    release_dates = models.DateField()
+    title = models.CharField(max_length=50)
+    #ratings.critics_score
+    rt_rating = models.IntegerField()
+    synopsis = models.TextField()
+
+    #setter and getter for cast, which is a list of 
+    #actor JSON objects, like so 
+    #[ {"name": "Lily James", "characters": ["Ella"],"id": "771402472"},...]
+    def setCast(self, x):
+        self.cast = json.dumps(x)
+
+    def getCast(self, x):
+        return json.loads(self.cast)    
