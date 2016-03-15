4e30f3762a89b4dba54dfd255ca13d48d4375512
Dan <17@dlock.tech>
2016-03-15 17:30:35 -0300

    got to part 3

--- dlockdottech/RecentMediaScraper/admin.py
+++ dlockdottech/RecentMediaScraper/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Anime
+from .models import TVShow
+from .models import Movie
 
 # Register your models here.
+admin.site.register(Anime)
+admin.site.register(TVShow)
+admin.site.register(Movie)
--- dlockdottech/RecentMediaScraper/models.py
+++ dlockdottech/RecentMediaScraper/models.py
@@ -2,15 +2,20 @@ from django.db import models
 
 # Create your models here.
 class Media_Base(models.Model):
+    class Meta:
+        abstract = True
     title = models.CharField(max_length=300)
-    release_date = models.DateTimeField()
+    release_date = models.DateTimeField('date released')
     rating = models.DecimalField(max_digits=5, decimal_places=2)
 
+    def __str__(self):
+        return self.title
+
 class Movie(Media_Base):
-    return
+    lead_actors = models.CharField(max_length=300)
 
 class TVShow(Media_Base):
-    return
+    lead_actors = models.CharField(max_length=300)
 
 class Anime(Media_Base):
-    return
+    animation_studio = models.CharField(max_length=50)
